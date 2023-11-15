f89151da4db71ea87900f7d68c9e393272b858f2
Nagaraju Mogili <Nagaraju_Mogili@Persistent.com>
2023-11-15 11:23:55 +0530

    imperativeMethodParams

--- force-app/main/default/lwc/errorPanel/errorPanel.js
+++ force-app/main/default/lwc/errorPanel/errorPanel.js
@@ -1,5 +1,5 @@
-import { LightningElement,track } from 'lwc';
+import { LightningElement,api } from 'lwc';
 
 export default class ErrorPanel extends LightningElement {
-    @track errors
+    @api errors
 }
