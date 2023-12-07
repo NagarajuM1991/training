71bb31bc74249affd9aac913985022b87d55b0cb
Nagaraju Mogili <Nagaraju_Mogili@Persistent.com>
2023-12-07 21:58:04 +0530

    latest changes

---
+++ force-app/main/default/lwc/lmsComponentA/lmsComponentA.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import { APPLICATION_SCOPE,createMessageContext, MessageContext, publish, releaseMessageContext, subscribe, unsubscribe} from 'lightning/messageService';
+//import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel';
+/* https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_salesforce_modules */
+
+export default class LmsComponentA extends LightningElement {
+    
+}
---
+++ force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class LmsComponentX extends LightningElement {
+    
+}
